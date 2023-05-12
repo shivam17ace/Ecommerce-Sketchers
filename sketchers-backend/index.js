d1428dc4b55be5ba11dd90363784afb82fbcef77
shivam17acelucid <shivam.rawat@acelucid.com>
2023-05-13 00:08:45 +0530

    Initial Commit

---
+++ sketchers-backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoSanitize = require("express-mongo-sanitize");
+const mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+require("dotenv").config();
+const Route = require("./Routes/index");
+
+/* middleware */
+app.use(bodyparser.json());
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(cors());
+app.use("/", Route);
+
+app.get('/', (req, res) => {
+  res.send("Sketchers Backend Is Running")
+})
+
+/* mongodb connection */
+mongoose
+  .connect(process.env.DATABASE)
+  .then(console.log("database connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+/* server */
+const port = process.env.PORT;
+
+app.listen(port, (req, res, next) => {
+  console.log(`PORT is running on ${port}`);
+});
